0a6f8849c660815157f85875dfb3ed01bdf10ac9
Jawaria Alvi <jawariaalvi32@gmail.com>
2020-11-10 01:07:58 +0500

    Handled rendeing same products repeatedly

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -8,6 +8,8 @@ export default (state = INITIAL_STATE, action) => {
     console.log("A => ",action.payload)
     switch(action.type) {
         case "SETPRODUCTS" :
+            state.pro.length = 0;
+
             return ({
                 ...state,
                 pro:  [...state.pro, action.payload]
