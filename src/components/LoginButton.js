884379c1d6d6aefafc5375ff06badacd9fd4a794
jawariaalvi32 <jawariaalvi32@gmail.com>
2020-12-28 01:27:51 +0500

    Sell Button Link added

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -3,16 +3,23 @@ import '../App.css';
 import { BsChat } from 'react-icons/bs'
 import { MdNotificationsNone } from 'react-icons/md'
 import avatar from '../images/avatar_3.png'
-import { RiArrowDropDownLine } from 'react-icons/ri'
-
+import {Dropdown} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 class LoginButton extends React.Component { 
   render() {
     return (
-      <div>
-          < BsChat size="25" className="ml-2"/>
-          < MdNotificationsNone size="25" className="ml-2"/>
-          <img src={ avatar } alt="Profie" width="40px" className="ml-2"/>
-          < RiArrowDropDownLine size="30"/>
+      <div className="d-flex">
+          < BsChat size="25" className="ml-2 mt-2"/>
+          < MdNotificationsNone size="25" className="ml-2 mt-2"/>
+          <Dropdown>
+            <Dropdown.Toggle className="btn-login" id="dropdown-basic">
+              <img src={ avatar } alt="Profie" width="40px" className="ml-2"/>
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item><Link to="/sell">Sell Product</Link></Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Logout</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
       </div>
     );
   }
